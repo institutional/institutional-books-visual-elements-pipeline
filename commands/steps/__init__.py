978c6a1ef30cacc0e62bd1bffa241cdbc475ebcb
Jimmy-Mendez <jmendez@law.harvard.edu>
2025-11-07 13:49:01 -0500

    adding classification step

--- commands/steps/__init__.py
+++ commands/steps/__init__.py
@@ -1,5 +1,6 @@
 import click
 from .step01_detect import step01_detect
+from .step02_classify import step02_classify
 
 
 @click.group("steps")
@@ -8,3 +9,4 @@ def steps():
 
 
 steps.add_command(step01_detect)
+steps.add_command(step02_classify)
