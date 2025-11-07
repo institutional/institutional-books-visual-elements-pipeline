65a86997dabc95ecc80522920b6d68e36e989bfb
Jimmy-Mendez <jmendez@law.harvard.edu>
2025-11-07 13:47:17 -0500

    adding classification model

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .pipeline_run import PipelineRun
 from .pipeline_batch import PipelineBatch
 from .pipeline_batch_item import PipelineBatchItem
 from .detection import Detection
+from .classification import Classification
