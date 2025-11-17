1e18ab9a00819b90576cae907beb15eed1c63f4a
Jimmy-Mendez <jmendez@law.harvard.edu>
2025-11-17 14:18:16 -0500

    adding caption steo

--- commands/steps/__init__.py
+++ commands/steps/__init__.py
@@ -2,6 +2,7 @@ import click
 from .step01_detect import step01_detect
 from .step02_classify import step02_classify
 from .step03_generate_dedupe_embeddings import step03_generate_dedupe_embeddings
+from .step04_generate_caption_requests import step04_generate_caption_requests
 
 
 @click.group("steps")
@@ -12,3 +13,4 @@ def steps():
 steps.add_command(step01_detect)
 steps.add_command(step02_classify)
 steps.add_command(step03_generate_dedupe_embeddings)
+steps.add_command(step04_generate_caption_requests)
