f28573555e4be1121f08ceeb90b7729a5a9c4bd5
Jimmy-Mendez <jmendez@law.harvard.edu>
2025-11-25 10:17:53 -0500

    removing batch models

--- models/__init__.py
+++ models/__init__.py
@@ -7,3 +7,4 @@ from .classification import Classification
 from .embedding import Embedding
 from .imagehash import ImageHash
 from .caption import Caption
+from .deduped_embedding import DedupedEmbedding
