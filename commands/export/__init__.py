8a98c6c9d6f62fec7e9da585ad502085a2230fd7
Jimmy-Mendez <jmen34956@gmail.com>
2026-05-07 15:04:45 +0000

    add all commands

--- commands/export/__init__.py
+++ commands/export/__init__.py
@@ -2,6 +2,12 @@ import click
 from .peek import peek
 from .analyze import analyze
 from .stats import stats
+from .backfill import backfill
+from .filter_dataset import filter_dataset
+from .viewer_gui import viewer_gui
+from .embedding_atlas import embedding_atlas
+from .caption_accuracy import caption_accuracy
+from .count_tokens import count_tokens
 
 
 @click.group("export")
@@ -12,3 +18,9 @@ def export():
 export.add_command(peek)
 export.add_command(analyze)
 export.add_command(stats)
+export.add_command(backfill)
+export.add_command(filter_dataset)
+export.add_command(viewer_gui)
+export.add_command(embedding_atlas)
+export.add_command(caption_accuracy)
+export.add_command(count_tokens)
