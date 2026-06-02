cd9881bbd96029b17e1cf98d71b49e1f2d6b4f4d
Jimmy-Mendez <jmen34956@gmail.com>
2026-06-02 15:05:36 +0000

    add new stuff

--- commands/export/__init__.py
+++ commands/export/__init__.py
@@ -3,7 +3,7 @@ from .peek import peek
 from .stats import stats
 from .backfill import backfill
 from .to_s3 import to_s3
-from .viewer_gui import viewer_gui
+from .viewer_space import viewer_space
 from .embedding_atlas import embedding_atlas
 from .count_tokens import count_tokens
 from .to_hf import to_hf
@@ -17,8 +17,8 @@ def export():
 export.add_command(peek)
 export.add_command(stats)
 export.add_command(backfill)
-export.add_command(filter_dataset)
-export.add_command(viewer_gui)
+export.add_command(to_s3)
+export.add_command(viewer_space)
 export.add_command(embedding_atlas)
 export.add_command(count_tokens)
 export.add_command(to_hf)
