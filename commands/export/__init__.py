3d469089b35809548a5f3e9324ea442d4995aef4
Jimmy-Mendez <jmen34956@gmail.com>
2026-06-03 15:09:24 +0000

    moving post-processing out

--- commands/export/__init__.py
+++ commands/export/__init__.py
@@ -1,11 +1,8 @@
 import click
 from .peek import peek
 from .stats import stats
-from .backfill import backfill
 from .to_s3 import to_s3
 from .viewer_space import viewer_space
-from .embedding_atlas import embedding_atlas
-from .count_tokens import count_tokens
 from .to_hf import to_hf
 
 
@@ -16,9 +13,6 @@ def export():
 
 export.add_command(peek)
 export.add_command(stats)
-export.add_command(backfill)
 export.add_command(to_s3)
 export.add_command(viewer_space)
-export.add_command(embedding_atlas)
-export.add_command(count_tokens)
 export.add_command(to_hf)
