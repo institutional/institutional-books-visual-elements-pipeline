8f0d2f55f844169f6855aa46a48d44384f4b39b8
Jimmy Mendez <jmen34956@gmail.com>
2026-06-25 19:24:11 +0000

    update data dump

--- data_dump.sh
+++ data_dump.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd /home/idi-dev/institutional-books-1-ve-pipeline
-
 source .env
 
-mkdir -p "${DATA_DIR_PATH}/database-dumps"
-SQL_OUTPUT_FILE="${DATA_DIR_PATH}/database-dumps/$(date '+%Y-%m-%d-%H-%M').sql"
+mkdir -p "database-dumps"
+SQL_OUTPUT_FILE="database-dumps/$(date '+%Y-%m-%d-%H-%M').sql"
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 
-/usr/bin/pg_dump \
+/usr/lib/postgresql/17/bin/pg_dump \
   --dbname="$POSTGRES_DB" \
   --host="$POSTGRES_HOST" \
   --port="$POSTGRES_PORT" \
@@ -18,7 +16,6 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
   --no-owner \
   --no-acl \
   --format=p \
-  --table caption \
   > "$SQL_OUTPUT_FILE"
 
 echo "Database dump created at: $SQL_OUTPUT_FILE"
